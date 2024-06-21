b247f27cf73a30be6384b89db74e14ae24ee8c9c
namtranviet269 <namtranviet2609@gmail.com>
2024-06-22 02:02:32 +0700

    update product_detail

--- lib/client/screens/product_detail_screen.dart
+++ lib/client/screens/product_detail_screen.dart
@@ -30,56 +30,64 @@ class ProductDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 255, 92, 52),
         title: SearchWidget(),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: Image.network(
-                product.imageUrl,
-                fit: BoxFit.cover,
-                height: 200,
-                width: double.infinity,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Image.network(
+                  product.imageUrl,
+                  fit: BoxFit.cover,
+                  height: 200,
+                  width: double.infinity,
+                ),
               ),
-            ),
-            SizedBox(height: 16),
-            Text(
-              product.name,
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8),
-            Text(
-              '\$${product.price.toStringAsFixed(2)}',
-              style: TextStyle(fontSize: 20, color: Colors.red),
-            ),
-            SizedBox(height: 8),
-            ContainerButton(
-              label: 'Mua ngay',
-              onPressed: () {
-                //xu ly chuyen trang thanh toan
-              },
-              icon: Icons.shopping_cart_rounded,
-            ),
-            SizedBox(height: 8),
-            ContainerButton(
-              label: 'Thêm vào giỏ',
-              onPressed: () {
-                //xu ly chuyen trang gio hang
-              },
-              icon: Icons.add_shopping_cart_sharp,
-            ),
-            SizedBox(height: 16),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                product.description,
-                style: TextStyle(fontSize: 16),
+              SizedBox(height: 16),
+              Text(
+                product.name,
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
-            ),
-          ],
+              SizedBox(height: 8),
+              Text(
+                '\$${product.price.toStringAsFixed(2)}',
+                style: TextStyle(fontSize: 20, color: Colors.red),
+              ),
+              SizedBox(height: 8),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ContainerButton(
+                    label: 'Mua ngay',
+                    onPressed: () {
+                      //xu ly chuyen trang thanh toan
+                    },
+                    icon: Icons.shopping_cart_rounded,
+                  ),
+                  ContainerButton(
+                    label: 'Thêm vào giỏ',
+                    onPressed: () {
+                      //xu ly chuyen trang gio hang
+                    },
+                    icon: Icons.add_shopping_cart_sharp,
+                  ),
+                ],
+              ),
+              SizedBox(height: 16),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  product.description,
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       // bottomSheet: NavBar(),
--- lib/client/widgets/button_custom.dart
+++ lib/client/widgets/button_custom.dart
@@ -16,33 +16,32 @@ class ContainerButton extends StatelessWidget {
     return GestureDetector(
       onTap: onPressed,
       child: Container(
+        width: MediaQuery.sizeOf(context).width * 0.45,
         padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
         decoration: BoxDecoration(
           color: const Color.fromARGB(255, 255, 92, 52),
           borderRadius: BorderRadius.circular(8),
         ),
-        child: Center(
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              if (icon != null) ...[
-                Icon(
-                  icon,
-                  color: Colors.white,
-                ),
-                const SizedBox(width: 8), // Khoảng cách giữa icon và label
-              ],
-              Text(
-                label,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            if (icon != null) ...[
+              Icon(
+                icon,
+                color: Colors.white,
               ),
+              const SizedBox(width: 8), // Khoảng cách giữa icon và label
             ],
-          ),
+            Text(
+              label,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 10,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
         ),
       ),
     );
