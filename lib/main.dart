3ac31e96970e455c7700c483dac47c5c0b4294ea
namtranviet269 <namtranviet2609@gmail.com>
2024-06-22 23:46:07 +0700

    update server

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/client/screens/product_detail_screen.dart';
+import 'package:flutter_application_1/client/widgets/nav.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,15 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My App',
-      home: ProductDetailScreen(
-        product: Product(
-            name: 'sp001',
-            imageUrl:
-                'https://product.hstatic.net/200000722513/product/pc_case_xigmatek_-_26_50758d07dff4461ebd00809e2699e2e0_grande.png',
-            price: 100000000,
-            description:
-                'CPU Intel Core i5-12400F, nhân tố khuất đảo thị trường PC Gaming khi sở hữu mức giá rẻ cùng hiệu năng xuất sắc. Với 6 nhân 12 luồng, xung nhịp 2.5GHz và turbo boost lên 4.4 GHz, quả là sự lựa chọn tuyệt vời từ khả năng chơi game cho tới stream game của thế hệ vi xử lý Intel Gen 12, chính là sự nâng cấp vượt bậc so với người tiền nhiệm i5-11400F.'),
-      ),
+      home: NavBar(),
       debugShowCheckedModeBanner: false,
     );
   }
