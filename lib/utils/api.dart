3df8f5893fe87552c52746d563d527b2d329fadc
namtranviet269 <namtranviet2609@gmail.com>
2024-07-02 02:27:51 +0700

    update user_sevice

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -1,4 +1,4 @@
 class Api {
-  static const String userServiceUrl = 'http://192.168.42.183:3000/userservice';
-  static const String baseUrl = 'http://192.168.42.183:3000';
+  static const String userServiceUrl = 'http://192.168.1.13:3000/userservice';
+  static const String baseUrl = 'http://192.168.1.13:3000';
 }
