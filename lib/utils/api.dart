287410c95943710c6f59abd2ff7a2737316dffc8
namtranviet269 <namtranviet2609@gmail.com>
2024-07-15 10:21:23 +0700

    update

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -1,9 +1,9 @@
 class Api {
-  static const String userServiceUrl = 'http://192.168.1.12:3000/userservice';
-  static const String baseUrl = 'http://192.168.1.12:3000';
-  static const String orderService = 'http://192.168.1.12:3000/order';
+  static const String userServiceUrl = 'http://192.168.0.113:3000/userservice';
+  static const String baseUrl = 'http://192.168.0.113:3000';
+  static const String orderService = 'http://192.168.0.113:3000/order';
   static const String orderDetailsService =
-      'http://192.168.1.12:3000/order_details';
+      'http://192.168.0.113:3000/order_details';
   static const String notificationService =
-      'http://192.168.1.12:3000/notification';
+      'http://192.168.0.113:3000/notification';
 }
